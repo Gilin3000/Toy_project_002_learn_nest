7571a5cb61795a23cc8594a76e8527c21fa2e22e
Gilinsik <lotus0110@nate.com>
2022-12-22 00:53:06 +0900

    feat : app.ts - READ cats, cats/:id

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,36 +1,63 @@
+//** Create Read */
+
 import * as express from "express";
 import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
-const data = [1, 2, 3, 4];
 
+//* logging middleware
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   console.log("this is logging middleware");
   next();
 });
 
-app.get("/cats/som", (req, res, next) => {
-  console.log("this is som middleware");
-  next();
+//* READ 고양이 전체 데이터 다 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    // throw new Error('db connect error');
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+//* READ 특정 고양이 데이터 조회
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log(params);
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    // throw new Error('db connect error');
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
-});
-
-app.get("/cats/blue", (req, res) => {
-  res.send({ blue: Cat[0] });
-});
-
-app.get("/cats/som", (req, res) => {
-  res.send({ som: Cat[1] });
-});
-
+//* 404 middleware
 app.use((req, res, next) => {
-    console.log("this is error middleware");
-    res.send({error: '404 not found error'});
-  });
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
+});
 
 app.listen(8000, () => {
   console.log("server is on...");
