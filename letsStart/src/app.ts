4b5dfdd741a997496773cb86eb77fc62e0139c1b
Gilinsik <lotus0110@nate.com>
2022-12-21 23:26:18 +0900

    feat : src - app.model.ts,  app.get

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,13 +1,14 @@
-import * as express from 'express';
+import * as express from "express";
+import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
-const data = [1,2,3,4];
+const data = [1, 2, 3, 4];
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    console.log(req);
-    res.send({data});
-})
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat });
+});
 
 app.listen(8000, () => {
-    console.log('server is on...');
-})
+  console.log("server is on...");
+});
