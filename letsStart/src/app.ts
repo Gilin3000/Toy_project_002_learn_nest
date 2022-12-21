243f3c7ca7ad4f3582643a98aa8f4a7169770e8d
Gilinsik <lotus0110@nate.com>
2022-12-21 23:41:33 +0900

    feat : app.use middleware

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -4,19 +4,32 @@ import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
 const data = [1, 2, 3, 4];
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
+});
+
+app.get("/cats/som", (req, res, next) => {
+  console.log("this is som middleware");
+  next();
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
 });
 
 app.get("/cats/blue", (req, res) => {
-  console.log(req.rawHeaders[1]);
   res.send({ blue: Cat[0] });
 });
 
 app.get("/cats/som", (req, res) => {
-    console.log(req.rawHeaders[1]);
-    res.send({ som: Cat[1] });
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+    console.log("this is error middleware");
+    res.send({error: '404 not found error'});
   });
 
 app.listen(8000, () => {
