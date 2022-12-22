778a6a3986d00d306bd46a0d2c4cf7958d832b8c
Gilinsik <lotus0110@nate.com>
2022-12-22 09:44:12 +0900

    feat : put with forEach

--- letsStart/src/cats/cats.route.ts
+++ letsStart/src/cats/cats.route.ts
@@ -1,5 +1,5 @@
 import { Cat, CatType } from "./cats.model";
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
@@ -61,5 +61,35 @@ router.post("/cats", (req, res) => {
     });
   }
 });
+
+//* UPDATE 고양이 데이터 업데이트 -> PUT
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+        if (cat.id === params.id){
+            cat = body;
+            result = cat;
+        }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* UPDATE 고양이 데이터 부분 업데이트 -> PATCH
+
+//* DELETE 고양이 데이터 삭제 -> DELETE
 
 export default router;
