8fb5f91f5c0b5c3a2704c4cda87e952f252b5a5b
Gilinsik <lotus0110@nate.com>
2022-12-24 12:42:01 +0900

    feat : cats.controller 일부작성

---
+++ project/src/cats/cats.controller.ts
@@ -0,0 +1,41 @@
+import { createCat, deleteCat, updateParticalCat } from './../../../letsStart/src/cats/cats.service';
+import { Controller } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+    constructor(private readonly catsService: CatsService){}
+
+    // cats/
+    @Get()
+    getAllCat(){
+        return 'all cat';
+    }
+
+    // cats/:id
+    @Get(':id')
+    getOneCat(){
+        return 'one cat';
+    }
+
+    @Post()
+    createCat{
+        return 'update cat';
+    }
+
+    @Put(':id')
+    updateCat(){
+        return 'update cat';
+    }
+
+    @Patch(':id')
+    updateParticalCat(){
+        return 'update';
+    }
+
+    @delete(':id')
+    deleteCat(){
+        return 'delete service';
+    }
+
+}
