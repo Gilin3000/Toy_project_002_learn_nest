992d62720b5b16a724bb60f2c7f25880bf398c38
Gilinsik <lotus0110@nate.com>
2022-12-25 11:58:46 +0900

    feat : exception filter, pipes

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,20 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { HttpExceptionFilter } from './../http-exception.filter';
+import { Controller, HttpException, UseFilters, Param, ParseIntPipe } from '@nestjs/common';
 import { Delete, Patch, Get, Post, Put } from '@nestjs/common';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
-
+  
   // cats/
   @Get()
   getAllCat() {
+    // throw new HttpException('api broken', 401);
     return 'all cat';
   }
 
   // cats/:id
   @Get(':id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
     return 'one cat';
   }
 
