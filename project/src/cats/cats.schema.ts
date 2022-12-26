789bb59a2f3147642e440348c80307c0109e404b
Gilinsik <lotus0110@nate.com>
2022-12-26 10:07:17 +0900

    feat : DB schema

---
+++ project/src/cats/cats.schema.ts
@@ -0,0 +1,39 @@
+import { CatSchema } from './../../node_modules/@nestjs/mongoose/tests/src/cats/schemas/cat.schema';
+import { Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Cat extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: number;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop()
+  @IsString()
+  imgUrl: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
