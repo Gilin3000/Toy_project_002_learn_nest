e46ce687e97543ad9697dd370ba441149ae838cd
Gilinsik <lotus0110@nate.com>
2022-12-27 17:19:17 +0900

    fix : existByEmail - return bool, add : auth module

--- project/src/cats/cats.repository.ts
+++ project/src/cats/cats.repository.ts
@@ -1,24 +1,24 @@
 import { CatRequestDto } from './dto/cats.request.dto';
-import { HttpException } from '@nestjs/common';
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { model } from 'mongoose';
+import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
 
 @Injectable()
 export class CatsRepository {
-  constructor(@InjectModel(Cat.name) private catModel: model<Cat>) {}
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
 
   async existsByEmail(email: string): Promise<boolean> {
     try {
-      const result = await this.catModel.exist({ email });
-      return result;
+      const result = await this.catModel.exists({ email });
+      if (result) return true;
+      else return false;
     } catch (error) {
       throw new HttpException('db error', 400);
     }
   }
 
-  async create(cat:CatRequestDto): Promise<Cat>{
+  async create(cat: CatRequestDto): Promise<Cat> {
     return await this.catModel.create(cat);
   }
 }
