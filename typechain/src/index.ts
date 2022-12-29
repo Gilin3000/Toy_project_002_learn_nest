8957bf4019cf7722db4066e6cd46f081e9287810
Gilinsik <lotus0110@nate.com>
2022-12-29 15:39:11 +0900

    feat : blockchain typescript

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,10 +1,52 @@
-import { init, exit } from "./myPackage";
-
-class Block{
-    constructor(private data: string) {}
-    static hello(){
-        return "hi"
+import crypto from "crypto";
+interface BlockShape{
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+class Block implements BlockShape{
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ){
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(preveHash: string, height: number, data: string): string{
+        const toHash = `${preveHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
-console.log(Block.hello())
 
+class Blcochain{
+    private blocks: Block[];
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if(this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length -1].hash;
+    }
+    public addBlock(data: string){
+        const newBlock = new Block(
+            this.getPrevHash(),
+            this.blocks.length + 1,
+            data
+        );
+        this.blocks.push(newBlock);
+    }
+    public getBlocks(){
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blcochain();
+
+blockchain.addBlock("first block");
+blockchain.addBlock("second block");
+blockchain.addBlock("third block");
+blockchain.addBlock("fourth block");
+
+console.log(blockchain.getBlocks());
---
+++ typechain/study/index.ts
@@ -0,0 +1,10 @@
+import { init, exit } from "./myPackage";
+
+class Block{
+    constructor(private data: string) {}
+    static hello(){
+        return "hi"
+    }
+}
+console.log(Block.hello())
+
