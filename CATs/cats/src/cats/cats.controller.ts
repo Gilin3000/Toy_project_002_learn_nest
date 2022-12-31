4611edb18bccd50afa5dcbdad7489067562fb71f
Gilinsik <lotus0110@nate.com>
2022-12-31 18:43:56 +0900

    feat : http-exception

--- CATs/cats/src/cats/cats.controller.ts
+++ CATs/cats/src/cats/cats.controller.ts
@@ -1,12 +1,25 @@
+import { HttpExceptionFilter } from './../http-exception.filter';
 import { CatsService } from './cats.service';
-import { Controller, Put, Get, Post, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Put,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  HttpException,
+  Param,
+  UseFilters,
+} from '@nestjs/common';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCat() {
+    throw new HttpException('api broken', 401);
     return 'all cat';
   }
 
