0e14cb12bcd4c9ee85775510df1b56dc5bcaee90
Gilinsik <lotus0110@nate.com>
2023-01-02 01:10:21 +0900

    feat : mysql db init

--- Prisma/with-prisma/src/app.module.ts
+++ Prisma/with-prisma/src/app.module.ts
@@ -1,10 +1,10 @@
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaService],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
