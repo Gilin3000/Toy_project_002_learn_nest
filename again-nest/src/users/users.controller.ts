0c43c2f07fc8adfc2904e7631bdf9f10ffeda2ec
Gilin3000 <lotus0110@nate.com>
2023-03-13 01:52:50 +0900

    feat : users controller

--- again-nest/src/users/users.controller.ts
+++ again-nest/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { CreateUserDto } from 'src/dto/create-user.dto';
 
 @Controller('users')
@@ -6,5 +6,23 @@ export class UsersController {
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
     console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
   }
 }
