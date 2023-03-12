75f8799aac58a7f15bb5db035a7430b493e2a2bc
Gilin3000 <lotus0110@nate.com>
2023-03-13 01:41:53 +0900

    feat : create user func

--- again-nest/src/users/users.controller.ts
+++ again-nest/src/users/users.controller.ts
@@ -2,3 +2,12 @@ import { Controller } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {}
+import { Controller, Post, Body } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+  }
+}
