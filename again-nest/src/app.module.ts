5eb3d4b3d596749d0e30db49216bf1940c3b83ac
Gilin3000 <lotus0110@nate.com>
2023-03-13 18:51:57 +0900

    feat : use config package

--- again-nest/src/app.module.ts
+++ again-nest/src/app.module.ts
@@ -1,10 +1,24 @@
+import { AppService } from './../../Prisma/with-prisma/src/app.service';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : process.env.NODE_ENV === 'stage'
+          ? '.stage.env'
+          : '.development.env',
+    }),
+    UsersModule,
+    EmailModule,
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
