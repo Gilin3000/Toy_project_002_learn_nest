3e1c0154ececff90f90f5e8dbddf83c1da51d802
Gilin3000 <lotus0110@nate.com>
2023-03-13 14:15:28 +0900

    feat : email service

--- again-nest/src/app.module.ts
+++ again-nest/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { EmailService } from './email/email.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, EmailService],
 })
 export class AppModule {}
