a642a1fc7b526be17c4c9ecf55c19914514b462a
Gilin3000 <lotus0110@nate.com>
2023-03-13 17:17:51 +0900

    feat : user module

--- again-nest/src/app.module.ts
+++ again-nest/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
