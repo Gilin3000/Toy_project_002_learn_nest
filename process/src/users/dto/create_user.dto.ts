810e8ff5a99e023f13fa52f1c0dcf260e3ad010f
Gilin3000 <lotus0110@nate.com>
2023-03-14 00:19:28 +0900

    feat : class validator

--- process/src/users/dto/create_user.dto.ts
+++ process/src/users/dto/create_user.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, MinLength, MaxLength, IsEmail } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(20)
   name: string;
+
+  @IsEmail()
   email: string;
   password: string;
 }
