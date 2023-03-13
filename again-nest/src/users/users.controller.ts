5de7a161a7b6bee7f8d8372d6a8d4881e1619bc5
Gilin3000 <lotus0110@nate.com>
2023-03-13 10:02:08 +0900

    feat : users service

--- again-nest/src/users/users.controller.ts
+++ again-nest/src/users/users.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Post, Get, Body, Query, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Query,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { VerifyEmailDto } from 'src/users/dto/verify-email.dto';
 import { UserLoginDto } from 'src/users/dto/user-login.dto';
 import { UserInfo } from 'src/users/interface/userinfo.interface';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
     console.log(dto);
@@ -27,5 +38,10 @@ export class UsersController {
   async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
     console.log(userId);
     return;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }
