6a14c79217bbe45e68e5bc103b819801e3cef610
Gilin3000 <lotus0110@nate.com>
2023-03-14 10:17:07 +0900

    feat : import typeorm mysql

--- process/src/app.module.ts
+++ process/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,16 @@ import { UsersModule } from './users/users.module';
       load: [emailConfig],
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
   ],
   controllers: [AppController],
